2a432d6addbc64105fb5938437091233a0b4d796
Cary Jones <cgjonesdev@gmail.com>
2018-01-29 18:47:58 -0600

    Added endpoints and templates

--- app/urls.py
+++ app/urls.py
@@ -15,5 +15,14 @@ app.permanent_session_lifetime  = timedelta(
 # Main resolvers
 ##############################################################################
 app.add_url_rule('/', view_func=base.IndexView.as_view('index'))
+app.add_url_rule('/signup', view_func=base.SignupView.as_view('signup'))
+app.add_url_rule('/login', view_func=base.LoginView.as_view('login'))
+app.add_url_rule('/donate', view_func=base.DonateView.as_view('donate'))
 app.add_url_rule('/needs', view_func=base.NeedsView.as_view('needs'))
 app.add_url_rule('/volunteers', view_func=base.VolunteersView.as_view('volunteers'))
+app.add_url_rule('/contributors', view_func=base.ContributorsView.as_view('contributors'))
+app.add_url_rule('/resources', view_func=base.ResourcesView.as_view('resources'))
+app.add_url_rule('/collaborators', view_func=base.CollaboratorsView.as_view('collaborators'))
+app.add_url_rule('/teams', view_func=base.TeamsView.as_view('teams'))
+app.add_url_rule('/liaison', view_func=base.LiaisonView.as_view('liaison'))
+app.add_url_rule('/events', view_func=base.EventsView.as_view('events'))
